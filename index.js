c90a43aedae17a2f09ca39465ed37c35c1128d4a
sethvincent <sethvincent@gmail.com>
2026-03-24 16:48:43 -0700

    🚀

---
+++ index.js
@@ -0,0 +1,8 @@
+const platform = require('#bluetooth')
+
+exports.Central = platform.Central
+exports.Peripheral = platform.Peripheral
+exports.Server = platform.Server
+exports.L2CAPChannel = platform.L2CAPChannel
+exports.Service = platform.Service
+exports.Characteristic = platform.Characteristic
